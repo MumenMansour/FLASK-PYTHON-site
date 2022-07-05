b0b5d2e709cc43a83895645043884a95007d2831
Mumen Mansour <103611001+MumenMansour@users.noreply.github.com>
2022-07-05 20:03:42 -0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+app = Flask(__name__) #Instanciando classe Flask.
+
+#Endereço do site.
+@app.route('/') #Rota principal.
+@app.route('/index') #Rota alternativa.
+def index():
+    #return "Hello, world!"
+    nome='Mumen' #Variável.
+    dados={"idade":"25 anos", "cidade":"Santa Rosa"} #Variável.
+    return render_template('index.html',nome=nome, dados=dados)
+@app.route('/contato')
+def contato():
+        return render_template('contato.html')
+@app.route('/produtos')
+def produtos():
+        return render_template('produtos.html')
+
+
+
+
+
+
+
+app.run()
